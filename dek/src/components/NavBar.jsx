2f84ca541827d11d4b230515cce27373003998ed
Daniel Lopez <lopezdaniel2604@gmail.com>
2025-09-22 12:22:56 -0700

    Added more files and folders for orginization and added some code

---
+++ dek/src/components/NavBar.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { useEffect } from "react";
+
+const navItems = [
+    {name: "Profile", href: "#profile"},
+    {name: "Stats", href: "#stats"}
+]
