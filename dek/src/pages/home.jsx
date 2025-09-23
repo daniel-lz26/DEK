c6ecda61c42951db19af24cc20df072b8a2f063e
kwitecki <kaylawitecki@gmail.com>
2025-09-23 16:55:38 -0700

    created pages and nav links very basic pages with basic descriptions

--- dek/src/pages/home.jsx
+++ dek/src/pages/home.jsx
@@ -1,13 +1,13 @@
 import { Navbar } from "../components/NavBar"
 import { Logo } from "../assets/Logo"
 
-
 export const Home = () => {
-
-    return ( <div className="min-h-screen bg-background text-foreground overflow-x-hidden font-black"> 
-        
-        <Navbar />
-
-    </div>
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden font-black">
+            <Navbar />
+            <main className="pt-28 px-4">
+                <h1 className="text-3xl mb-4">Home</h1>
+            </main>
+        </div>
     )
 }
