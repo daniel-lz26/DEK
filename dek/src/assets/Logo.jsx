e402431b31631260a1c142b09427cd1af724f4da
Daniel Lopez <lopezdaniel2604@gmail.com>
2025-09-23 13:38:27 -0700

    logo and home changes

---
+++ dek/src/assets/Logo.jsx
@@ -0,0 +1,8 @@
+
+
+export const Logo = () => {
+    return (<div className="logo w-16 h-16">
+        <img src="./assets/images/DEK.png" alt="logo"/>
+    </div>
+    )
+}
--- dek/src/pages/home.jsx
+++ dek/src/pages/home.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from "../components/NavBar"
+import { Logo } from "../assets/Logo"
 
 
 export const Home = () => {
 
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
+    return ( <div className="min-h-screen bg-background text-foreground overflow-x-hidden font-black"> 
         
         <Navbar />
 
     </div>
+    )
 }
