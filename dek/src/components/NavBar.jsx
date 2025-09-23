6d889b5f788cc939a0393b150e06566c388c1442
Kayla Witecki <kwitecki@csu.fullerton.edu>
2025-09-23 13:13:40 -0700

    added Navigation bar and created style guide for it

--- dek/src/components/NavBar.jsx
+++ dek/src/components/NavBar.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
-import { Menu, X } from "lucide-react";
-import { useEffect } from "react";
-import { cn } from "../lib/utils";
-
 const navItems = [
-    {name: "Profile", href: "#profile"},
-    {name: "Stats", href: "#stats"},
-    {name: "Friends", href: "#friends"},
-    {name: "shuffle", href: "#shuffle"}
-]
+  { name: "Profile", href: "#profile" },
+  { name: "Stats", href: "#stats" },
+  { name: "Friends", href: "#friends" },
+  { name: "Shuffle", href: "#shuffle" },
+];
 // makng a putting the entire navbar into one varible to call it to the home file
 export const Navbar = () => {
-    <header className="bg-dark-background sticky top-0 z-[20] mx-auto flex w-full items-center justify-between border-b border-gray-500 p-8">
-        TEST TEST TEST
-        <h1>
-            NavBar TEST TEST TEST
-        </h1>
-    </header>
-}
+  return (
+    <div className="fixed top-5 right-5 nav-style" >
+      <nav className="flex items-center bg-transparent">
+        {navItems.map((item) => (
+          <a
+            key={item.name}
+            href={item.href}
+            className="nav-text hover:underline px-2"
+          >
+            {item.name}
+          </a>
+        ))}
+      </nav>
+    </div>
+  );
+};
